d2ccba5b3212916cc0836afa2d5f0b689e80ce03
GravyDown <145372183+GravyDown@users.noreply.github.com>
2023-09-25 23:02:19 +0530

    Perfect_number.py
    Print the list of perfect number upto the user-provided number

---
+++ Perfect_number.py
@@ -0,0 +1,19 @@
+#program for perfect number
+print("Values >15,000 may take forever ;)")
+n=int(input("Enter upper limit: "))
+l2=[]
+for i in range(2,n):
+    l1=[]
+    for j in range(1,i):
+        if i%j==0:
+            l1.append(j)
+    s=sum(l1)
+    if i==s:
+        l2.append(i)
+if (len(l2)==0):
+    print("empty")
+else:
+    print("Perfect numbers upto",n,"are:",l2)
+            
+
+        
